cefa3295a01be306852637235fb324d779b19d3c
James Hubert <jwhubert91@gmail.com>
2022-03-20 00:20:31 -0700

    Add toggle, data, and begin state work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
+import { useState } from 'react';
+
+// components
+import Toggle from 'react-toggle'
+
+// data
+import { vocabulary } from './vocabulary'
+
 function App() {
+  const { part1, part2, part3 } = vocabulary;
+
+  const [part1Text, setPart1Text] = useState('');
+  const [part2Text, setPart2Text] = useState('');
+  const [part3Text, setPart3Text] = useState('');
+
+  function retrievePhrases() {
+    setPart1Text( part1[0].english );
+    setPart2Text( part2[0].english );
+    setPart3Text( part3[0].english );
+  }
+
+  function printPhrases() {
+    setTimeout(()=> {console.log( `${ part1Text } ${ part2Text } ${ part3Text }` )}, 2000)
+  }
+
   return (
-    <div className="h-screen w-full text-center p-4 flex flex-col">
+    <div className="h-screen w-full text-center p-6 flex flex-col">
       <header className="mb-5">
         <h1 className="text-xl font-bold">Winter 2022 Speed Spanish</h1>
         <h2>Week 1 - The Magic Circle</h2>
       </header>
-      <main className="flex-1">
-        <div className="border-2 p-2">
-          <p>Toggle English or Spanish</p>
-          <p>Button for a new phrase</p>
-          <p>Three parts to the magic circle go here</p>
+      <main className="w-1/2 flex flex-col justify-center align-middle flex-1">
+        <div className="border-2 px-6 py-8">
+          <label className="flex w-full align-middle justify-center mb-4">
+            <span>English</span>
+              <Toggle
+                icons={false}
+                className="mx-3"
+              />
+              <span>Español</span>
+            </label>
+          <button
+            className="
+            rounded-full
+            bg-cyan-500
+            hover:bg-cyan-600
+            px-6 py-2
+            text-white
+            border-2
+            border-cyan-700
+            mb-4
+            "
+            onClick={retrievePhrases}
+          >
+            New phrase!
+          </button>
+          <div className="w-full flex justify-center">
+            <span>Part 1</span>
+            <span>Part 2</span>
+            <span>Part 3</span>
+          </div>
         </div>
       </main>
     </div>
---
+++ src/vocabulary.js
@@ -0,0 +1,188 @@
+export const vocabulary = {
+  part1: [
+    {
+      id: 1,
+      english: 'It is necessary',
+      spanish: 'Es necesario',
+    },
+    {
+      id: 2,
+      english: 'It is important',
+      spanish: 'Es importante',
+    },
+    {
+      id: 3,
+      english: 'It is possible',
+      spanish: 'Es posible',
+    },
+    {
+      id: 4,
+      english: 'It is impossible',
+      spanish: 'Es posible',
+    },
+    {
+      id: 5,
+      english: 'It is easy',
+      spanish: 'Es fácil',
+    },
+    {
+      id: 6,
+      english: 'It is difficult',
+      spanish: 'Es difícil',
+    },
+    {
+      id:7,
+      english: 'I would like',
+      spanish: 'Me gustaría',
+    },
+    {
+      id:8,
+      english: 'I want',
+      spanish: 'Quiero',
+    },
+    {
+      id:9,
+      english: 'She wants',
+      spanish: 'Quiere',
+    },
+    {
+      id:10,
+      english: 'He wants',
+      spanish: 'Quiere',
+    },
+    {
+      id:11,
+      english: 'I have to',
+      spanish: 'Tengo que',
+    },
+    {
+      id:12,
+      english: 'She has to',
+      spanish: 'Tiene que',
+    },
+    {
+      id:13,
+      english: 'He has to',
+      spanish: 'Tiene que',
+    },
+  ],
+  part2: [
+    {
+      id:14,
+      english: 'to give',
+      spanish: 'dar',
+    },
+    {
+      id:15,
+      english: 'to rent',
+      spanish: 'rentar',
+    },
+    {
+      id:16,
+      english: 'to use',
+      spanish: 'usar',
+    },
+    {
+      id:17,
+      english: 'to speak',
+      spanish: 'hablar',
+    },
+    {
+      id:18,
+      english: 'to work',
+      spanish: 'trabajar',
+    },
+    {
+      id:19,
+      english: 'to practice',
+      spanish: 'practicar',
+    },
+    {
+      id:20,
+      english: 'to pay',
+      spanish: 'pagar',
+    },
+    {
+      id:21,
+      english: 'to help',
+      spanish: 'ayudar',
+    },
+    {
+      id:22,
+      english: 'to study',
+      spanish: 'estudiar',
+    },
+    {
+      id:23,
+      english: 'to fill out',
+      spanish: 'llenar',
+    },
+    {
+      id:24,
+      english: 'to buy',
+      spanish: 'comprar',
+    },
+  ],
+  part3: [
+    {
+      id: 25,
+      english: 'me',
+      spanish: 'me',
+    },
+    {
+      id: 26,
+      english: 'it',
+      spanish: 'la',
+    },
+    {
+      id: 27,
+      english: 'her',
+      spanish: 'la',
+    },
+    {
+      id: 28,
+      english: 'because',
+      spanish: 'porque',
+    },
+    {
+      id: 29,
+      english: 'the house',
+      spanish: 'la casa',
+    },
+    {
+      id: 30,
+      english: 'the table',
+      spanish: 'la mesa',
+    },
+    {
+      id: 31,
+      english: 'the chair',
+      spanish: 'la silla',
+    },
+    {
+      id: 32,
+      english: 'the shower',
+      spanish: 'la ducha',
+    },
+    {
+      id: 33,
+      english: 'the window',
+      spanish: 'la ventana',
+    },
+    {
+      id: 34,
+      english: 'the kitchen',
+      spanish: 'la cocina',
+    },
+    {
+      id: 35,
+      english: 'the bedroom',
+      spanish: 'el alcoba',
+    },
+    {
+      id: 36,
+      english: 'the light',
+      spanish: 'la luz',
+    },
+  ],
+};
